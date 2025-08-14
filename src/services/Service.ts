f89720cf92416bd98bb7fac359c73328ae7393a6
Eduardo Garcia <147555023+oligEdu@users.noreply.github.com>
2025-08-13 21:45:00 -0300

    Update Service.ts

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'baseURL: import.meta.env.VITE_API_URL'
+    baseURL: import.meta.env.VITE_API_URL'
 })
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
